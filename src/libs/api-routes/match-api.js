6d1929547f660278997c21a404c4a9221ce4f433
rafi_alam <rafi@shellbeehaken.com>
2024-05-13 17:45:27 +0600

    next url

--- src/libs/api-routes/match-api.js
+++ src/libs/api-routes/match-api.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
-const api_endpoint = '/api/score';
-const API = axios.create({ baseURL: "http://localhost:3000" })
+const api_endpoint = "/api/score";
+const API = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_API_URL });
 
-API.interceptors.request.use((req)=>{
-    req.headers["Content-Type"] = "application/json";
-    return req;
-})
+API.interceptors.request.use((req) => {
+  req.headers["Content-Type"] = "application/json";
+  return req;
+});
 
 API.interceptors.response.use((res) => {
-    return res
+  return res;
 });
 
 // export const createScore = (payload) =>API.post("/api/createScore", payload);
 // export const updateScore = (payload) =>API.patch(`/api/updateScore/${payload._id}`, payload.updatedFields)
 // export const getScore = (payload) =>API.get(`/api/getScore/${payload}`);
-export const createScore = (payload) =>API.post(api_endpoint, payload);
-export const updateScore = (payload) =>API.patch(api_endpoint, payload.updatedFields)
-export const getScore = (payload) =>API.get(api_endpoint, {params:{matchId: payload}});
+export const createScore = (payload) => API.post(api_endpoint, payload);
+export const updateScore = (payload) =>
+  API.patch(api_endpoint, payload.updatedFields);
+export const getScore = (payload) =>
+  API.get(api_endpoint, { params: { matchId: payload } });
